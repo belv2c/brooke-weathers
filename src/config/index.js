ed491b45e3a4a6b5d897894a07d82092f42b6830
Brooke Lewis <belv2c@gmail.com>
2019-12-17 22:10:10 -0600

    initial setup

---
+++ src/config/index.js
@@ -0,0 +1,53 @@
+module.exports = {
+    siteTitle: 'Brooke Weathers | Software Developer',
+    siteDescription:
+      'Brooke Weathers is a software engineer based in Nashville, TN.',
+    siteKeywords:
+      'Brooke Weathers, brooklynlew, belv2c, software developer, web developer, javascript, nashville',
+    siteUrl: 'https://brooklynlew.com',
+    siteLanguage: 'en_US',
+    googleAnalyticsID: '',
+    googleVerification: '',
+    name: 'Brooke Weathers',
+    location: 'Nashville, TN',
+    email: 'belv2c@gmail.com',
+    github: 'https://github.com/belv2c',
+    twitterHandle: '@brooklynloo',
+    socialMedia: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/belv2c',
+      },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/belv2c',
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/brooklynlew',
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/brooklynloo',
+      },
+    ],
+  
+    navLinks: [
+      {
+        name: 'About',
+        url: '/#about',
+      },
+      {
+        name: 'Experience',
+        url: '/#jobs',
+      },
+      {
+        name: 'Work',
+        url: '/#projects',
+      },
+      {
+        name: 'Contact',
+        url: '/#contact',
+      },
+    ]
+  };
